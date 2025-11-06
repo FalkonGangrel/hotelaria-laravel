20dd49864ac08b1ac9f78f9552e3619a7ed176d2
FalkonGangrel <falkon.gangrel@gmail.com>
2025-11-06 11:38:32 -0300

    feat: Create product listing endpoint with Controller and Route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController; // <-- IMPORTANTE: Importe o Controller
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+// Define uma rota do tipo GET para o endereço '/produtos'.
+// Quando essa rota for acessada, ela executará o método 'index'
+// da classe 'ProdutoController'.
+Route::get('/produtos', [ProdutoController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
